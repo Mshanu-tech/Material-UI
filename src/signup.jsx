a9d9aa15fd80b94481c2d27457f6eda667aea393
Mshanu-tech <ashanu2004@gmail.com>
2023-12-08 10:34:40 +0530

    Initial commit

---
+++ src/signup.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Button from '@mui/material/Button';
+
+
+function signup() {
+  return (
+    <>
+    <h1>Hello</h1>
+    <Button variant="contained">Hello world</Button>
+    </>
+  )
+}
+
+export default signup
