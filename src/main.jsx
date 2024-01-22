d12cc223958fce604d59c455e8f365a4ca2c1798
Mshanu-tech <muhammedshanu12345678910@gmail.com>
2024-01-22 19:54:15 +0530

    start learning mui

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import SignInSide from './signup.jsx'
-import './index.css'
+import { App } from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <SignInSide />
+    < App />
   </React.StrictMode>,
 )
